114b5f2818cb12791bd33bf90baf62f39abaa35d
Allan Salazar <allansalazar@mac.mynetworksettings.com>
2025-12-03 23:27:59 -0500

    change IP

--- phishguard/src/api.ts
+++ phishguard/src/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-const BASE = "http://132.238.110.162:8000";
+const BASE = "http://127.0.0.1:8000";
 const TOKEN_KEY = "phishguard_token";
 const ROLE_KEY = "phishguard_role";
 
